339d9698ebe128436a6691940bae74c94a75cbc4
Hardyanto <xiau.chien@hotmail.com>
2017-05-19 14:38:41 +0800

    first commit

---
+++ DemoTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication1;
+namespace DemoTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Tutorial tp = new Tutorial();
+            Assert.AreEqual(tp.Name, "Continuous Integration");
+        }
+    }
+}
---
+++ WebApplication1/Tutorial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Tutorial
+    {
+        public String Name;
+        public Tutorial()
+        {
+            Name = "Continuous Integration";
+        }
+    }
+}
